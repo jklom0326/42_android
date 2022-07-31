4d137423cd69d3496439e02e2d9d2b7e2392a2fb
jklom0326 <jklom0326@gmail.com>
2022-07-31 23:50:26 +0900

    context_test

--- 07_17_subject/app/src/main/java/com/example/android_42_1/MainActivity.kt
+++ 07_17_subject/app/src/main/java/com/example/android_42_1/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.android_42_1
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -16,9 +18,9 @@ class MainActivity : AppCompatActivity() {
         val context3 = (view.context == applicationContext) // 3
         val context4 = (view.context == application) // 4
 
-        println(context1)
-        println(context2)
-        println(context3)
-        println(context4)
+        Log.d(TAG, "1st=" + (context1))
+        Log.d(TAG, "2nd=" + (context2))
+        Log.d(TAG, "3rd=" + (context3))
+        Log.d(TAG, "4th=" + (context4))
     }
 }
